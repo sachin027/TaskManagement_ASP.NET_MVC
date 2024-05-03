ff6afd0784f43e0f954ab59be0ca780417dd67d9
Sachin Kanzariya <sachin.k@shaligraminfotech.com>
2024-05-03 17:05:30 +0530

    login practice

--- TaskManagement/Controllers/LoginSignupController.cs
+++ TaskManagement/Controllers/LoginSignupController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TaskManagement.Models;
 
 namespace TaskManagement.Controllers.LoginSignup
 {
@@ -13,7 +14,20 @@ namespace TaskManagement.Controllers.LoginSignup
         {
             return View();
         }
-
+        [HttpPost]
+        public String Index(LoginDetailsModel loginDetails)
+        {
+            string name = "";
+            if (loginDetails.UserRole == "Student")
+            {
+                name = "Student";
+            }
+            else
+            {
+                name = "Teacher";
+            }
+            return name;
+        }
         public ActionResult RegistrationPage()
         {
             return View();
