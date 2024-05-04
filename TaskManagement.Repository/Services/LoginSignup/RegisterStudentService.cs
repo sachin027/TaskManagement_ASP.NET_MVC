45083a8a5f9917e9f45a0b450c9cd50d976f8ac0
Sachin Kanzariya <sachin.k@shaligraminfotech.com>
2024-05-04 13:12:38 +0530

    Registration lnterface and service create

---
+++ TaskManagement.Repository/Services/LoginSignup/RegisterStudentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Models.DBContext;
+using TaskManagement.Models.ViewModel;
+
+namespace TaskManagement.Repository.Interface.LoginSignupInterface
+{
+    public class RegisterStudentService : ILoginSignupInterface
+    {
+        public Students RegisterStudent(StudentModel studentModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
