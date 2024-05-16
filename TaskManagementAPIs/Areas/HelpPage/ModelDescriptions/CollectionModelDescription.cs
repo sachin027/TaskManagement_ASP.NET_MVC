0e4dbbe795113b3a956e32ae2f553219ea96082f
Sachin Kanzariya <sachin.k@shaligraminfotech.com>
2024-05-16 18:20:24 +0530

    API

---
+++ TaskManagementAPIs/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TaskManagementAPIs.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TaskManagementAPIs/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TaskManagementAPIs.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
